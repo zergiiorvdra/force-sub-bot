8f1bd41f44972f6fa3d389f098ea9b49145007e8
zer-Pyro <143489626+zergiiorvdra@users.noreply.github.com>
2024-03-02 01:15:49 +0800

    Add files via upload

---
+++ Data.py
@@ -0,0 +1,46 @@
+# Credits: @mrismanaziz
+# FROM File-Sharing-Man <https://github.com/mrismanaziz/File-Sharing-Man/>
+# t.me/SharingUserbot & t.me/Lunatic0de
+
+from pyrogram.types import InlineKeyboardButton
+
+class Data:
+    HELP = """
+<b> ❏ Perintah untuk Pengguna BOT
+ ├ /start - Mulai Bot
+ ├ /about - Tentang Bot ini
+ ├ /help - Bantuan Perintah Bot ini
+
+
+👨‍💻 Organized by </b><a href='https://t.me/CTVASELOLE'>@CTVASELOLE</a>
+"""
+
+    close = [
+        [InlineKeyboardButton("ᴛᴜᴛᴜᴘ", callback_data="close")]
+    ]
+
+    mbuttons = [
+        [
+            InlineKeyboardButton("ʜᴇʟᴘ & ᴄᴏᴍᴍᴀɴᴅs", callback_data="help"),
+            InlineKeyboardButton("ᴛᴜᴛᴜᴘ", callback_data="close")
+        ],
+    ]
+
+    buttons = [
+        [
+            InlineKeyboardButton("ᴛᴇɴᴛᴀɴɢ sᴀʏᴀ", callback_data="about"),
+            InlineKeyboardButton("ᴛᴜᴛᴜᴘ", callback_data="close")
+        ],
+    ]
+
+    ABOUT = """
+<b>Tentang Bot ini:
+
+@{} adalah Bot Telegram untuk menyimpan Postingan atau File yang dapat Diakses melalui Link Khusus.
+
+ • Creator: @{}
+ • Framework: <a href='https://docs.pyrogram.org'>Pyrogram</a>
+ • Source Code: <a href='https://github.com/mrismanaziz/File-Sharing-Man'>File-Sharing-Man v4</a>
+
+👨‍💻 Organized by </b><a href='https://t.me/CTVASELOLE'>@CTVASELOLE</a>
+"""
